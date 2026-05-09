af9f142d16e3c6b922cb181a511ea8da05258f9f
Nawar-Adriouch <nawaradriouch5@gmail.com>
2026-05-09 18:49:08 +0100

    Modifications

--- docs/nawar_codes/mon-laravel/app/Models/Categorie.php
+++ docs/nawar_codes/mon-laravel/app/Models/Categorie.php
@@ -15,4 +15,17 @@ class Categorie extends Model
     {
         return $this->hasMany(Annonce::class);
     }
+     public function create()
+{
+    $categories = Categorie::all();
+
+    return view('annonces.create', compact('categories'));
+}
+
+public function edit(Annonce $annonce)
+{
+    $categories = Categorie::all();
+
+    return view('annonces.edit', compact('annonce', 'categories'));
+}
 }
--- docs/nawar_codes/mon-laravel/database/seeders/CategorieSeeder.php
+++ docs/nawar_codes/mon-laravel/database/seeders/CategorieSeeder.php
@@ -11,16 +11,9 @@ class CategorieSeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['nom' => 'Immobilier',       'icone' => '🏠'],
-            ['nom' => 'Véhicules',        'icone' => '🚗'],
-            ['nom' => 'Électronique',     'icone' => '📱'],
-            ['nom' => 'Vêtements',        'icone' => '👗'],
-            ['nom' => 'Maison & Jardin',  'icone' => '🛋️'],
+            ['nom' => 'Ventes',       'icone' => '💰'],
             ['nom' => 'Emploi',           'icone' => '💼'],
             ['nom' => 'Services',         'icone' => '🔧'],
-            ['nom' => 'Sports & Loisirs', 'icone' => '⚽'],
-            ['nom' => 'Animaux',          'icone' => '🐾'],
-            ['nom' => 'Autres',           'icone' => '📦'],
         ];
 
         foreach ($categories as $cat) {
